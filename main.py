6b2c16546eb484bf5ea176fb47216e2e5cac43fe
Rohan-Thoma <rohanvailalathoma@gmail.com>
2025-06-26 13:43:07 +0530

    Data Ingestio pipeline is completed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.text_summarizer.logging import logger 
+from src.text_summarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("logging is implemented")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+
+    logger.info(f"stage {STAGE_NAME} Completed")
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
