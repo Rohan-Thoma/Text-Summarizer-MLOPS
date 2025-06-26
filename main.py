ffa1a7a256198ba2d1759c2be2b5747a030150c4
Rohan-Thoma <rohanvailalathoma@gmail.com>
2025-06-26 16:35:20 +0530

    Data transformation pipeline completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.text_summarizer.logging import logger 
 from src.text_summarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.text_summarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -9,8 +10,21 @@ try:
     data_ingestion_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
 
-    logger.info(f"stage {STAGE_NAME} Completed")
+    logger.info(f"stage {STAGE_NAME} is completed")
 
 except Exception as e:
     logger.exception(e)
     raise e 
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+   logger.info(f"stage {STAGE_NAME} initiated")
+
+   data_transformation_pipeline = DataTransformationTrainingPipeline()
+   data_transformation_pipeline.initiate_data_transformation()
+
+   logger.info(f"stage {STAGE_NAME} is completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
